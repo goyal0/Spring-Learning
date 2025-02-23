f4e30d29c6228771976f2dec110584d61c22b5f4
piyush goyal <piygoyal30@gmail.com>
2025-02-23 17:57:03 +0530

    [Piyush Goyal]Adding files of UC5_PutRequest

--- restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
+++ restAPI/src/main/java/com/springIntro/restAPI/Controller/APIController.java
@@ -32,4 +32,10 @@ public class APIController {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
 
     }
+    @PutMapping("hello/put/{firstName}/")
+
+    public String sayHello(@PathVariable String firstName, @RequestParam String lastName) {
+
+        return "Hello " + firstName + " " + lastName + " from BridgeLabz";
+    }
 }
